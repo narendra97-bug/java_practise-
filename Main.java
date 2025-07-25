4f7ad25b93a9ca319ab72c106ea4f1339e740273
narendra chaudhary <narendrachaudhary@192.168.1.8>
2025-07-26 04:28:22 +0530

    explaning input/output in java

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
+
+import java.util.*;
+
 public class Main {
    public static void main(String[] args) {
-         System.out.println("hello narendra ");
-
+      int a=5;
+         System.out.println( a);
+            Scanner inputScanner = new Scanner(System.in);
+            System.out.println(inputScanner.nextLine());
    }
+
 
 
 }

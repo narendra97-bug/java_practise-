2de9884774d465bdc1126c326cc3a87d672ba03a
narendra chaudhary <narendrachaudhary@192.168.1.8>
2025-07-26 04:48:33 +0530

    explaning typecasting

--- Main.java
+++ Main.java
@@ -5,10 +5,14 @@ public class Main {
    public static void main(String[] args) {
       int a=5;
          System.out.println( a);
-            Scanner inputScanner = new Scanner(System.in);
-            System.out.println(inputScanner.nextLine());
+            try (Scanner inputScanner = new Scanner(System.in)) {
+                  System.out.println(inputScanner.nextLine());
+            }
+            String str = "Hello, World!";
+            System.out.println(str.isEmpty());
    }
 
 
 
 }
+
---
+++ Typecasting.java
@@ -0,0 +1,5 @@
+public class Typecasting {
+   public static void main(String[] args) {
+      System.out.println("typecasting bw int and string ");
+   }
+   }

1732f6481cc8940cd4b934c5d3f36b21ea8e0a34
narendra chaudhary <narendrachaudhary@192.168.1.8>
2025-08-01 04:21:01 +0530

    explaning input/output in java

--- Typecasting.java
+++ Typecasting.java
@@ -1,5 +1,16 @@
 public class Typecasting {
    public static void main(String[] args) {
       System.out.println("typecasting bw int and string ");
+      typecastingExample();
    }
+
+   static void typecastingExample() {
+      int num = 10;
+      String strNum = Integer.toString(num); // int to String
+      System.out.println("String representation: " + strNum);
+
+      String str = "20";
+      int parsedNum = Integer.parseInt(str); // String to int
+      System.out.println("Parsed integer: " + parsedNum);
    }
+}
